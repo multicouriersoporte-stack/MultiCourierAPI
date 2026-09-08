e977aa9d44cd97d4a036296f728649bd006c19e3
multicouriersoporte-stack <multicouriersoporte@gmail.com>
2026-09-07 23:57:19 -0500

    Update usuarios.routes.js

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+/* import { Router } from "express";
 import {
     getUsuarios, getUsuarioxid, getUsuarioPorCedula, getUsuarioPorEmail,
     getUsuarioPorCodigo, getRepartidorPorUsuario, postUsuarios, putUsuarios,
@@ -22,5 +22,33 @@ router.post("/", verificarToken, soloAdminCentral, postUsuarios);
 router.put("/:id", verificarToken, soloAdminCentral, putUsuarios);
 router.patch("/:id", verificarToken, soloAdminCentral, patchUsuarios);
 router.delete("/:id", verificarToken, soloAdminCentral, deleteUsuarios);
+
+export default router;
+ */
+
+import { Router } from "express";
+import {
+    getUsuarios, getUsuarioxid, getUsuarioPorCedula, getUsuarioPorEmail,
+    getUsuarioPorCodigo, getRepartidorPorUsuario, postUsuarios, putUsuarios,
+    patchUsuarios, deleteUsuarios
+} from "../controladores/usuariosCtrl.js";
+
+const router = Router();
+
+// Consultas
+router.get("/usuarios", getUsuarios);
+router.get("/usuarios/cedula/:cedula", getUsuarioPorCedula);
+router.get("/usuarios/email/:email", getUsuarioPorEmail);
+router.get("/usuarios/codigo/:codigo", getUsuarioPorCodigo);
+router.get("/usuarios/:id_usuario/repartidor", getRepartidorPorUsuario);
+router.get("/usuarios/:id", getUsuarioxid); // Después de las rutas específicas.
+
+// Crear y modificar
+router.post("/usuarios", postUsuarios);
+router.put("/usuarios/:id", putUsuarios);
+router.patch("/usuarios/:id", patchUsuarios);
+
+// Eliminar
+router.delete("/usuarios/:id", deleteUsuarios);
 
 export default router;
