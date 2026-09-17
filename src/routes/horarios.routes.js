798c63e1f2897700bd238393b22de002587ac7ab
multicouriersoporte-stack <multicouriersoporte@gmail.com>
2026-09-16 20:38:25 -0500

    Update horarios.routes.js

--- src/routes/horarios.routes.js
+++ src/routes/horarios.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { listarDisponibles, listarMisHoras, listarHistorial } from "../controladores/horariosCtrl.js";
 import { solicitarReserva, consultarSolicitud, soltarHoras } from "../controladores/reservasCtrl.js";
-import { ofrecer, listarOfertas, solicitar, aceptar, rechazar } from "../controladores/intercambiosCtrl.js";
+import { ofrecer, listarOfertas, misIntercambios, solicitar, aceptar, rechazar } from "../controladores/intercambiosCtrl.js";
 // import { verificarToken } from "../middlewares/auth.middleware.js";
 // import { permitirRoles } from "../middlewares/roles.middleware.js";
 
@@ -20,6 +20,7 @@ router.post("/reservas/:id/soltar", /* verificarToken, */ soltarHoras);
 // Intercambios
 router.post("/reservas/:id/ofrecer-intercambio", /* verificarToken, */ ofrecer);
 router.get("/intercambios/disponibles", /* verificarToken, */ listarOfertas);
+router.get("/intercambios/mis-intercambios", /* verificarToken, */ misIntercambios);
 router.post("/intercambios/:id/solicitar", /* verificarToken, */ solicitar);
 router.patch("/intercambios/:id/aceptar", /* verificarToken, */ aceptar);
 router.patch("/intercambios/:id/rechazar", /* verificarToken, */ rechazar);
