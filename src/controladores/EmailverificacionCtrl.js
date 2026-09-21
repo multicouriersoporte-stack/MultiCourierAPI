b242b47d4ac97ae2d3ff60eb036fd8ef693d7010
multicouriersoporte-stack <multicouriersoporte@gmail.com>
2026-09-21 05:03:02 -0500

    Update EmailverificacionCtrl.js

--- src/controladores/EmailverificacionCtrl.js
+++ src/controladores/EmailverificacionCtrl.js
@@ -138,3 +138,11 @@ export const correoFueVerificado = async (email) => {
     );
     return rows.length > 0;
 };
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    timezone: '-05:00'
+});
