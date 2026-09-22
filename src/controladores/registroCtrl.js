26c478bc02f5caef792783242406c2d5d473d0bb
multicouriersoporte-stack <multicouriersoporte@gmail.com>
2026-09-21 19:27:52 -0500

    Update registroCtrl.js

--- src/controladores/registroCtrl.js
+++ src/controladores/registroCtrl.js
@@ -155,7 +155,7 @@ export const registrarCliente = async (req, res) => {
 
 import bcrypt from "bcrypt";
 import { conmysql } from "../db.js";
-import { correoFueVerificado } from "./EmailVerificacionCtrl.js";
+import { correoFueVerificado } from "./EmailverificacionCtrl.js"
 
 // Rol asignado automáticamente al autorregistrarse como cliente.
 const ID_ROL_CLIENTE = 1;
