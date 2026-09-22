290b6f1b6c052e0c63a75aefb7d17d71f403cc56
multicouriersoporte-stack <multicouriersoporte@gmail.com>
2026-09-22 14:35:23 -0500

    Update pedidodetalles.routes.js

--- src/routes/pedidodetalles.routes.js
+++ src/routes/pedidodetalles.routes.js
@@ -11,7 +11,8 @@ const router = Router();
 // Rutas de consulta
 router.get("/pedidos-detalles", getPedidosDetalles);
 router.get("/pedidos-detalles/:id", getPedidoDetallePorId);
-router.get("/pedidos/:id_pedido/detalles", getDetallesPorPedido);
+//router.get("/pedidos/:id_pedido/detalles", getDetallesPorPedido);
+router.get("/pedidos/:id/detalles", verificarToken, permitirRoles(...ROLES_GET), getPedidoDetalles);
 router.get("/local-productos/:id_local_producto/pedidos-detalles", getDetallesPorLocalProducto);
 
 // Rutas CRUD
