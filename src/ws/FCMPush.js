e36b9d0e722c28fc75c5b4dc16bd514e38f10e52
multicouriersoporte-stack <multicouriersoporte@gmail.com>
2026-09-26 07:18:44 -0500

    Update FCMPush.js

--- src/ws/FCMPush.js
+++ src/ws/FCMPush.js
@@ -224,7 +224,7 @@ export default FOMPush;
 
 import { getFCMMessaging } from "../config/firebase-admin.js";
 
-const CANAL_NOTIFICACIONES = "orders_v1";
+const CANAL_NOTIFICACIONES = "orders_v2";
 
 // Envía un Push a un único dispositivo.
 export const enviarPush = async (token, { titulo = "MultiCourier", mensaje = "", datos = {}, sonido = "default" } = {}) => {
